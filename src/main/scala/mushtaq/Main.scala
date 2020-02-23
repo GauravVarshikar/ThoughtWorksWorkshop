c7e782c40531b94481b6c374fbdc08cba7b1be9b
GauravVarshikar <gaurav.varshikar@gmail.com>
2020-02-23 10:16:25 +0530

    Add files via upload

--- src/main/scala/mushtaq/Main.scala
+++ src/main/scala/mushtaq/Main.scala
@@ -1,9 +1,27 @@
 package mushtaq
 
+import java.util.concurrent.Executors
+
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+
+import scala.concurrent.ExecutionContext
+
 object Main {
 
   def main(args: Array[String]): Unit = {
-    println(123)
+
+    implicit val actorSystem: ActorSystem[Nothing] = ActorSystem(Behaviors.empty, "demo")
+    implicit val ec: ExecutionContext = ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(1))
+    val timerService = new TimerService()
+    var account: Account = new Account(new RbiStub(timerService))
+
+    account.stream.runForeach(println)
+
+    account.deposit(100)
+    account.deposit(200)
+    account.withdraw(130)
+
   }
 
 }
